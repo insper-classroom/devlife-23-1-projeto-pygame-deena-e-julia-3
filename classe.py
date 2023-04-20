b863424ad49c5c341909303596f5dcef995c7994
juliaas2 <juliaas@al.insper.edu.br>
2023-04-20 14:05:10 -0300

    classes

--- classe.py
+++ classe.py
@@ -1,6 +1,8 @@
 from funcoes import *
 import pygame 
 import random
+imagem_pomoouro=pygame.image.load(os.path.join('fotos','pixelado pomo de ouro.png'))
+imagem_torre=pygame.image.load(os.path.join('fotos',))
 #para conseguirmos fazer essa classe usamos como base o video https://www.youtube.com/watch?v=gomDSZaay3E e https://www.youtube.com/watch?v=WSPstecsF90.
 class Pomo_de_ouro_classico:
 
@@ -9,6 +11,7 @@ class Pomo_de_ouro_classico:
         self.posição_y=posição_y#altura do pomo 
         self.velocidade= 0#a velocidade que ele se encontra 
         self.angulo=0# o angulo dele para sabermos o angulo que ele ira voar
+        self.foto= imagem_pomoouro
 
     def voar(self):
         self.velocidade=-10#quando ele voar a velocidade precisa estar negativa para que ele não caia e sim cima
@@ -23,4 +26,11 @@ class Pomo_de_ouro_classico:
         self.posição_y+= estado #o passaro não muda de posição x ja que o movimento do passaro deve ser de cima para baixo logo quando isso acontesse a unica unidade modificada é o y 
 
 class Torre:
-    pass
+    distancia_entre_torres=150
+    velocidade=5
+    def __init__(self, posicao_x):
+        self.x=posicao_x
+        self.altura=0
+        self.parte_de_cima=0#da torre
+        self.parte_de_baixo=0#da torre
+        #self.imagem =
